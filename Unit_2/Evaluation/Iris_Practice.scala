8cbeee543ea1c0366024154760a0cf4637d979ea
Cristo JOsue <60414250+cristovazz172@users.noreply.github.com>
2021-05-29 14:44:27 -0700

    Insert the iris file and add the necessary libraries for the practice

---
+++ Unit_2/Evaluation/Iris_Practice.scala
@@ -0,0 +1,12 @@
+
+// In the following instructions we will return to some important imports for the realization of the problem and the perception of the data.
+
+// This data was retrieved from the repository: https://github.com/jcromerohdz/iris
+//We will use the Liberia Mlib that can help us in multilayer perception.
+// For the first step, you have to clean the data through this script.
+
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.feature. {VectorAssembler, StringIndexer}
+import org.apache.spark.ml.linalg.Vectors

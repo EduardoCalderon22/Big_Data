b03b4d2d2e26b7bac4a309e7a95caed2e788451b
Cristo JOsue <60414250+cristovazz172@users.noreply.github.com>
2021-05-30 19:07:02 -0700

    update code practice in data2 variable

--- Unit_2/Evaluation/Iris_Practice.scala
+++ Unit_2/Evaluation/Iris_Practice.scala
@@ -11,15 +11,15 @@ import org.apache.spark.ml.feature. {VectorAssembler, StringIndexer}
 import org.apache.spark.ml.linalg.Vectors
 
 // Loading data from iris data file.
-val data = spark.read.option ("header", "true"). option ("inferSchema", "true"). format ("csv"). load ("/home/eduardo/Escritorio/Datos/Big_Data/Unit_2/Evaluation/iris.csv")
+val data = spark.read.option ("header", "true").option("inferSchema", "true").format ("csv").load("C:/Repositorios/Big_Data/Unit_2/Evaluation/iris.csv")
 
 // Convertimos los datos de la columna "species" a valores numericos
 val label = new StringIndexer().setInputCol("species").setOutputCol("label")
 val labeltransform = label.fit(data).transform(data)
 
 // Convertimos los datos de todas las columnas a un arreglo llamadao "Features" y despues la pasamos a otro arreglo llamado data 2
-val Features = (new VectorAssembler (). setInputCols (Array ("sepal_length", "sepal_width", "petal_length", "petal_width")). setOutputCol ("features"))
-val data2 = Features.transform (labeltransform)
+val Features = (new VectorAssembler(). setInputCols (Array ("sepal_length", "sepal_width", "petal_length", "petal_width")).setOutputCol("features"))
+val data2 = Features.transform(labeltransform)
 
 // a. Utilice la librería Mllib de Spark el algoritmo de Machine Learning correspondiente a multilayer perceptron
     import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
@@ -33,26 +33,25 @@ val data2 = Features.transform (labeltransform)
 
 // 4. Imprime las primeras 5 columnas.
     data2.show(5)
-    data.select ($ "sepal_length", $ "sepal_width", $ "petal_length", $ "petal_width", $ "species"). show (5)
+    
 // 5. Usa el metodo describe () para aprender mas sobre los datos del DataFrame.
     data2.describe().show()
 
 // 6. Haga la transformación pertinente para los datos categoricos los cuales serán nuestras etiquetas a clasificar.
 
 // Cubre el algoritmo en trabajar con caractisticas, dividido en grupos de datos.
-    val speciesIndexer = new StringIndexer (). setInputCol ("species"). setOutputCol ("label")
-    val dataIndexed = speciesIndexer.fit (data2) .transform (data2)
+    val speciesIndexer = new StringIndexer().setInputCol ("species").setOutputCol ("label")
 
 // 7. Construya el modelo de clasificación y explique su arquitectura.
 // Splits aqui se utlizo para separar y para optener valores como archivos csv delimitados por tuberias.
-    val splits = dataIndexed.randomSplit (Array (0.6, 0.4), seed = 1234L)
+    val splits =  labeltransform.randomSplit (Array (0.6, 0.4), seed = 1234L)
 //Declarar el 60% de la informacion con la variable train.
     val train = splits (0)
 // Aqui se declara la otra infomacion de 40%.
     val test = splits (1)
 
     val layers = Array [Int] (4, 5, 4, 3)
-    val trainer = new MultilayerPerceptronClassifier (). setLayers (layers) .setBlockSize (128) .setSeed (1234L) .setMaxIter (100)
+    val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize (128) .setSeed (1234L) .setMaxIter (100)
     val model = trainer.fit (train)
     val result = model.transform (test)
     val predictionAndLabels = result.select ("prediction", "label")

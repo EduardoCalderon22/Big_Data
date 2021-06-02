55c2b9b0ee0bb148320bfa6f5e259a5b3f28a6f8
Cristo JOsue <60414250+cristovazz172@users.noreply.github.com>
2021-06-01 17:47:36 -0700

    polish the devaluation practice by adding more comments.

--- Unit_2/Evaluation/Iris_Practice.scala
+++ Unit_2/Evaluation/Iris_Practice.scala
@@ -1,23 +1,22 @@
+// En las siguientes instrucciones volveremos a algunas importaciones importantes para la realización del problema y la percepción de los datos.
 
-// In the following instructions we will return to some important imports for the realization of the problem and the perception of the data.
-
-// This data was retrieved from the repository: https://github.com/jcromerohdz/iris
-//We will use the Liberia Mlib that can help us in multilayer perception.
-// For the first step, you have to clean the data through this script.
+// Estos datos se recuperaron del repositorio: https://github.com/jcromerohdz/iris
+// Usaremos Liberia Mlib que puede ayudarnos en la percepción multicapa.
+// Para el primer paso, debe limpiar los datos a través de este script.
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature. {VectorAssembler, StringIndexer}
 import org.apache.spark.ml.linalg.Vectors
 
-// Loading data from iris data file.
+// Cargamos los datos del iris del .csv para ser evaluados.
 val data = spark.read.option ("header", "true").option("inferSchema", "true").format ("csv").load("C:/Repositorios/Big_Data/Unit_2/Evaluation/iris.csv")
 
 // Convertimos los datos de la columna "species" a valores numericos
 val label = new StringIndexer().setInputCol("species").setOutputCol("label")
 val labeltransform = label.fit(data).transform(data)
 
-// Convertimos los datos de todas las columnas a un arreglo llamadao "Features" y despues la pasamos a otro arreglo llamado data 2
+// Convertimos los datos de todas las columnas a un arreglo llamadao "Features" y despues la pasamos a otro arreglo llamado data 2.
 val Features = (new VectorAssembler(). setInputCols (Array ("sepal_length", "sepal_width", "petal_length", "petal_width")).setOutputCol("features"))
 val data2 = Features.transform(labeltransform)
 
@@ -42,21 +41,26 @@ val data2 = Features.transform(labeltransform)
 // Cubre el algoritmo en trabajar con caractisticas, dividido en grupos de datos.
     val data3 = data2.select("features", "label")
     data3.show()
-
+// Se separaremos los datos de entrenamientos y los datoss de testing usando los datos indexados dentro de la siguente linea.
     val splits = data3.randomSplit(Array(0.7, 0.3), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
 
-
 // 7. Construya el modelo de clasificación y explique su arquitectura.
     val layers = Array[Int](4, 5, 4, 3)
+// Se configura el entrenador del algoritmo Multilayer
     val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+// Se entrana el modelo, entrenado los datos del entrenamiento ejecutado con anterioridad
     val modelML = trainer.fit(train)
+// Se ejecutara un modelo con los datos de prueba.
     val result = modelML.transform(test)
     val predictionAndLabels = result.select("prediction", "label")
+// Se ejecuta la estimacion de los datos depediendo de la precision del modelo resultante.
     val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+//Mostramos la prediccion
     predictionAndLabels.show (50)
+//Mostramos los datos del resultado generado. 
     result.show (30)
 
-// 8. Imprima los resultados del modelo
+// 8. Resultado de impricion de los resultados del modelo.
     println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
